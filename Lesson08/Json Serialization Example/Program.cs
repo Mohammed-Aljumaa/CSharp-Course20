b269aa92a2a8fce7a4af31e70db2da95ac2e313a
MJ <MohammedAljumaaDev@gmail.com>
2026-07-02 14:27:08 +0300

    Add Lesson08 - Serialization Examples

---
+++ Lesson08/Json Serialization Example/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Json;
+
+
+[Serializable]
+
+public class Person
+{
+
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+
+
+}
+
+public class Program
+{
+    static void Main()
+    {
+        Person person = new Person();
+        person.Name = "Mohammed";
+        person.Age = 77;
+
+
+        DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(Person));
+        using (MemoryStream stream = new MemoryStream())
+        {
+
+            serializer.WriteObject(stream, person);
+
+            string jsonString =System.Text.Encoding.UTF8.GetString(stream.ToArray());
+
+
+            File.WriteAllText("Person.json", jsonString);
+
+
+
+        }
+
+
+        using (FileStream stream1 = new FileStream("Person.json", FileMode.Open))
+        {
+
+
+            Person deSerialized =(Person) serializer.ReadObject(stream1);
+
+            Console.WriteLine($"Name : {deSerialized.Name}, Age : {deSerialized.Age}" );
+        }
+
+
+
+    }
+
+
+
+}
---
+++ Lesson08/XML Serialization Example/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization; 
+
+
+[Serializable]
+public class Person
+{
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+
+
+
+
+}
+
+public class Program
+{
+    static  void Main()
+    {
+
+        Person person = new Person();
+        person.Name = "Mohammed";
+        person.Age = 33;
+
+
+        XmlSerializer serializer = new XmlSerializer(typeof(Person));
+        using (TextWriter writer =new StreamWriter("person.xml"))
+        {
+
+
+            serializer.Serialize(writer, person);
+
+        }
+
+
+        using(TextReader reader =new StreamReader("person.xml"))
+        {
+            Person deserializedPerson=(Person)serializer.Deserialize(reader);
+
+            Console.WriteLine($"Name : {deserializedPerson.Name},Age : {deserializedPerson.Age}");
+        }
+
+
+
+        
+    }
+
+
+
+
+}
