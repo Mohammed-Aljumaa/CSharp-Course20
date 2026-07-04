201f417b9824f43c16c380a404832e8e2a6bba40
MJ <MohammedAljumaaDev@gmail.com>
2026-07-04 14:09:49 +0300

    Add Lesson09 - Attribute Examples (Conditional, Obsolete, Custom)

---
+++ Lesson09/ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿//#define DEBUG
+#define koko
+
+
+using System;
+using System.Diagnostics;
+
+public class MyClass
+{
+
+    [Conditional("DEBUG")]
+    public void DebugMethode()
+    {
+
+        Console.WriteLine("Debug Method execute.");
+    }
+
+    public void NormalMethode()
+    {
+
+        Console.WriteLine("Normal Method execute.");
+
+    }
+
+
+
+
+
+}
+public class TraceExample
+{
+    [Conditional("koko")]
+    public static void LogTrace(string message)
+    {
+        Console.WriteLine($"[TRACE] {message}");
+    }
+}
+class program
+{
+
+    static void Main()
+    {
+        MyClass myClass = new MyClass();
+
+
+        myClass.DebugMethode();
+        myClass.NormalMethode();
+
+        TraceExample.LogTrace("This trace message will only be included if koko is defined.");
+        Console.WriteLine("Rest of the program.");
+
+
+
+
+    }
+}
---
+++ Lesson09/Custom Attribute/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+public class MyCustomAttribute : Attribute
+{
+    public string Description { get; }
+
+    public MyCustomAttribute(string description)
+    {
+        Description = description;
+    }
+}
+
+[MyCustom("This is a class attribute")]
+class MyClass
+{
+    [MyCustom("This is a method attribute")]
+    public void MyMethod()
+    {
+        Console.WriteLine("MyMethod is running...");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        MyClass obj = new MyClass();
+        obj.MyMethod();
+
+        Console.WriteLine("Program finished.");
+    }
+}
