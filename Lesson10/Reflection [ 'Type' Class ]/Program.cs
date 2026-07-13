563d7568cf78c1c7c953f1a5630dcb5dc46363f9
MJ <MohammedAljumaaDev@gmail.com>
2026-07-13 14:54:49 +0300

    Add Lesson10 - Reflection & Custom Attributes Examples

---
+++ Lesson10/Reflection [ 'Type' Class ]/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+using Print = System.Console;
+
+
+class program
+{
+    static void Main()
+    {
+        Type mytype=typeof(string);
+
+        Print.WriteLine("Type Information : ");
+
+        Print.WriteLine($"Name : {mytype.Name}");
+        Print.WriteLine($"FullName : {mytype.FullName}");
+        Print.WriteLine($"IsClass : {mytype.IsClass}");
+
+        
+
+
+
+    }
+}
---
+++ Lesson10/Reflection with Custom Attributes/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Reflection;
+
+
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+public class MyCustomAttribute : Attribute
+{
+    public string Description { get; }
+
+
+    public MyCustomAttribute(string description)
+    {
+        Description = description;
+    }
+}
+
+
+
+[MyCustom("This is a class attribute")]
+class MyClass
+{
+    [MyCustom("This is a method attribute")]
+    public void MyMethod()
+    {
+        // Method implementation
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Get the type of MyClass
+        Type type = typeof(MyClass);
+
+
+        // Get class-level attributes
+        object[] classAttributes = type.GetCustomAttributes(typeof(MyCustomAttribute), false);
+        foreach (MyCustomAttribute attribute in classAttributes)
+        {
+            Console.WriteLine($"Class Attribute: {attribute.Description}");
+        }
+
+
+        // Get method-level attributes
+        MethodInfo methodInfo = type.GetMethod("MyMethod");
+        object[] methodAttributes = methodInfo.GetCustomAttributes(typeof(MyCustomAttribute), false);
+        foreach (MyCustomAttribute attribute in methodAttributes)
+        {
+            Console.WriteLine($"Method Attribute: {attribute.Description}");
+        }
+    }
+}
