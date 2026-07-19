f5b9a8d6e5b802b90ff20ddfa135876c1745d87e
MJ <MohammedAljumaaDev@gmail.com>
2026-07-19 14:14:27 +0300

    Add Lesson12 - App.config, Registry operations, Logging, StringBuilder

---
+++ Lesson12/Logging Example/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+
+class Program
+{
+    static void Main()
+    {
+        // Specify the source name for the event log
+        string sourceName = "MyApp";
+
+
+        // Create the event source if it does not exist
+        if (!EventLog.SourceExists(sourceName))
+        {
+            EventLog.CreateEventSource(sourceName, "Application");
+            Console.WriteLine("Event source created.");
+        }
+
+
+        // Log an information event
+        EventLog.WriteEntry(sourceName, "This is an information event.", EventLogEntryType.Information);
+
+
+        Console.WriteLine("Event written to the log.");
+    }
+}
